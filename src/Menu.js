83590c7d8bda08a7ce049fb5197b1faa41e07151
Litkowczak <litkow@example>
2020-03-23 13:51:59 +0100

    Initialized project with default react app

---
+++ src/Menu.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Register from './Register'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Login from './Login';
+
+  function Menu() {
+      return (
+        <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      )
+  }
+  function Home() {
+    return <h2>Home</h2>;
+  }
+  
+
+  export default Menu;
