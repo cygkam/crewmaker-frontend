e94a352c9cf20c3a6e70a0d38dbfd0d84e7c48a8
Litkowczak <litkow@example>
2020-03-24 16:49:32 +0100

    Basic registration form

--- src/Register.js
+++ src/Register.js
@@ -1,13 +1,89 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-function Register() {
-  return (
-    <div>
-      <h1>
-        Formularz rejestracji...
-      </h1>
-    </div>
-  );
+class Register extends Component {
+
+  constructor(propos){
+    super(propos);
+    
+    this.state = {login: '',
+    password: '',
+    email: '',
+    name: '',
+    surname: '',
+    phonenumber: ''
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert("SIEMA")
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <div>
+        <form>
+            <label for="login">Login :</label>
+            <input
+                  type = "text"
+                  name = "login"
+                  value={this.state.login} onChange={this.handleChange}
+              />
+              <br/>
+            <label for="password">Hasło :</label>
+            <input
+                  type = "password"
+                  name = "password"
+                  value={this.state.password} onChange={this.handleChange}
+              />
+              <br/>
+              <label for="email">Email :</label>
+            <input
+                  type = "email"
+                  name = "email"
+                  value={this.state.email} onChange={this.handleChange}
+            />
+            <br/>
+            <label for="name">Imię :</label>
+            <input 
+                type = "text"
+                name = "name"
+                value = {this.state.name} onChange={this.handleChange}
+            >
+            </input>
+            <br/>
+            <label for="surname">Nazwisko :</label>
+            <input
+              type = 'text'
+              name = "surname"
+              value = {this.state.surname} onChange={this.handleChange}
+            />
+            <br/>
+            <label for="phonenumber">Telefon :</label>
+            <input
+              type ="text"
+              name = "phonenumber"
+              value = {this.state.phonenumber} onChange={this.handleChange}
+            />
+            <br/>
+          <input type="submit" value="Wyślij" onSubmit={this.handleSubmit} />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Register;
