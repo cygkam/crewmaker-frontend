e7319a45d463778679fc27111fc707efb47a171f
Maciej <libero2798@gmail.com>
2020-04-08 10:45:22 +0200

    adjust

--- src/MainProfilePage.js
+++ src/MainProfilePage.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Button,Label,ButtonContent,Icon, Form,Grid, GridColumn, Image,Header,Segment,Message, ButtonGroup, Modal } from 'semantic-ui-react'
+import { Button,Label,ButtonContent,Icon, Form,Grid, GridColumn, Image,Header,Segment,Message, ButtonGroup, Modal, GridRow } from 'semantic-ui-react'
 import UserInfo from "./UserInfo"
 import CommingEvent from "./CommingEvent"
 import PassedEvent from "./PassedEvent"
@@ -17,7 +17,11 @@ class MainProfilePage extends Component {
                 <Grid.Column width={5}>
                     <Segment>
                             <Label attached='top'>Twoje aktualne wydarzenia</Label>
-                            <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+                            <Grid>
+                                <GridRow>
+                                    <CommingEvent/>
+                                </GridRow>
+                            </Grid>
                     </Segment>
                     
                 </Grid.Column>
