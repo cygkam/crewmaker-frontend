3222e454057fe0a5b1db42d46ce3bd6d9790bf93
cygkam <cygkam@gmail.com>
2020-05-17 22:51:57 +0200

    added function changing event place archiving status

--- src/AdminPanel/EventPlaceAcceptingPanel/EventPlaceCard.js
+++ src/AdminPanel/EventPlaceAcceptingPanel/EventPlaceCard.js
@@ -5,6 +5,11 @@ import {
   GridRow,
   Button,
 } from "semantic-ui-react";
+import {eventPlaceService} from "../../Api/Api"
+import {notification} from "antd";
+import Switch from "@material-ui/core/Switch";
+import Typography from "@material-ui/core/Typography";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import { Link } from "react-router-dom";
 import bxBasketball from "@iconify/icons-bx/bx-basketball";
 import volleyballIcon from "@iconify/icons-uil/volleyball";
@@ -20,6 +25,22 @@ import roundDirectionsBike from "@iconify/icons-ic/round-directions-bike";
 import pingPongRacket from "@iconify/icons-si-glyph/ping-pong-racket";
 import { LineOutlined } from "@ant-design/icons";
 import { Icon } from "@iconify/react";
+import { grey } from "@material-ui/core/colors";
+import { withStyles } from "@material-ui/core/styles";
+
+const GreySwitch = withStyles({
+  switchBase: {
+    color: grey[300],
+    "&$checked": {
+      color: grey[600],
+    },
+    "&$checked + $track": {
+      backgroundColor: grey[600],
+    },
+  },
+  checked: {},
+  track: {},
+})(Switch);
 
 const toggleSportsCategories = [
   {
@@ -100,70 +121,135 @@ class EventPlaceCard extends Component {
       eventPlaceStreetNumber: "Undefined",
       eventPlacePostCode: "Undefined",
       eventPlaceCity: "Undefined",
-      eventPlaceSportsCategories: [ {sportsCategoryId: 1 }],
+      eventPlaceSportsCategories: [{ sportsCategoryId: 1 }],
       userRequestingID: null,
       userRequestingUsername: "Undefined",
       userAcceptingID: null,
       userAcceptingUsername: "Undefined",
       isAccepted: false,
+      isArchived: false,
       joinned: false,
       isLoading: false,
     };
+    this.handleAcceptEventPlace = this.handleAcceptEventPlace.bind(this);
+    this.handleToggleArchiveEventPlace = this.handleToggleArchiveEventPlace.bind(this);
   }
 
-    componentDidMount() {
-        this.setState({
-            eventPlaceID: this.props.eventPlaceDetails.eventPlaceID,
-            eventPlaceName: this.props.eventPlaceDetails.eventPlaceName,
-            eventPlaceDescription: this.props.eventPlaceDetails.eventPlaceDescription,
-            eventPlaceStreetName: this.props.eventPlaceDetails.eventPlaceStreetName,
-            eventPlaceStreetNumber: this.props.eventPlaceDetails
-            .eventPlaceStreetNumber,
-            eventPlacePostCode: this.props.eventPlaceDetails.eventPlacePostCode,
-            eventPlaceCity: this.props.eventPlaceDetails.eventPlaceCity,
-            eventPlaceSportsCategories: this.props.eventPlaceDetails.sportsCategories,
-            userRequestingUsername: this.props.eventPlaceDetails
-            .userRequestingUsername,
-            userAcceptingUsername: this.props.eventPlaceDetails.userAcceptingUsername,
-            isAccepted: this.props.eventPlaceDetails.isAccepted,
+  
+  handleToggleArchiveEventPlace() {
+    eventPlaceService
+      .archiveEventPlace(this.state.eventPlaceID, this.state.isArchived)
+      .then((response) => {
+        notification.success({
+          message: "Event place archive status",
+          description: "Thank you! Event place status has been changed.",
         });
-    }
+
+        this.setState({
+          isArchived: !this.state.isArchived,
+        });
+      })
+      .catch((error) => {
+        notification.error({
+          message: "Archived event place",
+          description:
+            error.message || "Sorry! Something went wrong. Please try again!",
+        });
+      });
+  }
+
+  handleAcceptEventPlace() {
+    eventPlaceService
+      .acceptEventPlace(this.state.eventPlaceID)
+      .then((response) => {
+        notification.success({
+          message: "Accepted new event place",
+          description: "Thank you! Event place has been accepted.",
+        });
+
+        this.setState({
+          isAccepted: response.isAccepted,
+          userAcceptingUsername: response.userAcceptingUsername,
+        });
+      })
+      .catch((error) => {
+        notification.error({
+          message: "Accept event place",
+          description:
+            error.message || "Sorry! Something went wrong. Please try again!",
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.setState({
+      eventPlaceID: this.props.eventPlaceDetails.eventPlaceID,
+      eventPlaceName: this.props.eventPlaceDetails.eventPlaceName,
+      eventPlaceDescription: this.props.eventPlaceDetails.eventPlaceDescription,
+      eventPlaceStreetName: this.props.eventPlaceDetails.eventPlaceStreetName,
+      eventPlaceStreetNumber: this.props.eventPlaceDetails
+        .eventPlaceStreetNumber,
+      eventPlacePostCode: this.props.eventPlaceDetails.eventPlacePostCode,
+      eventPlaceCity: this.props.eventPlaceDetails.eventPlaceCity,
+      eventPlaceSportsCategories: this.props.eventPlaceDetails.sportsCategories,
+      userRequestingUsername: this.props.eventPlaceDetails
+        .userRequestingUsername,
+      userAcceptingUsername: this.props.eventPlaceDetails.userAcceptingUsername,
+      isAccepted: this.props.eventPlaceDetails.isAccepted,
+      isArchived: this.props.eventPlaceDetails.isArchived,
+    });
+  }
 
   render() {
     return (
       <Grid divided>
-        <GridRow>
-          <GridColumn textAlign="center" verticalAlign="top" width={1}>
-            <h4 style={{ "font-weight": "bold" }}>ID:</h4>
+        <GridRow
+          style={
+            this.state.isArchived
+              ? { backgroundColor: "#EDEDED", color: "#EDEDED" }
+              : {}
+          }
+        >
+          <GridColumn
+            textAlign="center"
+            verticalAlign="top"
+            mobile={2}
+            tablet={2}
+            computer={1}
+          >
+            <h4 style={{ fontWeight: "bold" }}>ID:</h4>
             <h4>{this.state.eventPlaceID}</h4>
           </GridColumn>
-          <GridColumn verticalAlign="middle" width={3}>
-            <h4 style={{ "font-weight": "bold" }}>Nazwa obiektu:</h4>
+          <GridColumn verticalAlign="middle" mobile={6} tablet={4} computer={3}>
+            <h4 style={{ fontWeight: "bold" }}>Nazwa obiektu:</h4>
             <h4>{this.state.eventPlaceName}</h4>
-            <h4 style={{ "font-weight": "bold" }}>Opis:</h4>
+            <h4 style={{ fontWeight: "bold" }}>Opis:</h4>
             <h4>{this.state.eventPlaceDescription}</h4>
           </GridColumn>
-          <GridColumn verticalAlign="middle" width={3}>
-            <h4 style={{ "font-weight": "bold" }}>Miasto:</h4>
+          <GridColumn verticalAlign="middle" mobile={6} tablet={4} computer={3}>
+            <h4 style={{ fontWeight: "bold" }}>Miasto:</h4>
             <h4>
               {this.state.eventPlaceCity} {this.state.eventPlacePostCode}
             </h4>
-            <h4 style={{ "font-weight": "bold" }}>Ulica/Numer:</h4>
+            <h4 style={{ fontWeight: "bold" }}>Ulica/Numer:</h4>
             <h4>
               {this.state.eventPlaceStreetName}{" "}
               {this.state.eventPlaceStreetNumber}
             </h4>
           </GridColumn>
-          <GridColumn verticalAlign="middle" width={4}>
-            <h4 style={{ "font-weight": "bold" }}>Zgłaszający:</h4>
+          <GridColumn verticalAlign="middle" mobile={6} tablet={4} computer={3}>
+            <h4 style={{ fontWeight: "bold" }}>Zgłaszający:</h4>
             <h4>{this.state.userRequestingUsername}</h4>
-            <h4 style={{ "font-weight": "bold" }}>Zaakceptował:</h4>
+            <h4 style={{ fontWeight: "bold" }}>Zaakceptował:</h4>
             <h4>{this.state.userAcceptingUsername}</h4>
-            <h4 style={{ "font-weight": "bold" }}>
-              Zaakceptowane: {String(this.state.isAccepted)}
+            <h4 style={{ fontWeight: "bold" }}>
+              Zaakceptowane: {this.state.isAccepted ? "Tak" : "Nie"}
+            </h4>
+            <h4 style={{ fontWeight: "bold" }}>
+              Zarchiwizowane: {this.state.isArchived ? "Tak" : "Nie"}
             </h4>
           </GridColumn>
-          <GridColumn verticalAlign="middle" width={2}>
+          <GridColumn verticalAlign="middle" mobile={4} tablet={3} computer={2}>
             <div
               style={{
                 display: "flex",
@@ -177,6 +263,7 @@ class EventPlaceCard extends Component {
               {this.state.eventPlaceSportsCategories.map((item) => (
                 <React.Fragment>
                   <Icon
+                    color="black"
                     icon={
                       toggleSportsCategories[item.sportsCategoryId - 1].icon
                     }
@@ -190,10 +277,12 @@ class EventPlaceCard extends Component {
           </GridColumn>
 
           <GridColumn
-            centered
+            centered="true"
             textAlign="center"
             verticalAlign="middle"
-            width={2}
+            mobile={4}
+            tablet={4}
+            computer={2}
             padding={2}
           >
             <Grid.Row>
@@ -203,18 +292,32 @@ class EventPlaceCard extends Component {
                   size="small"
                   disabled={this.state.isAccepted}
                 >
-                  <Button.Content visible>Akceptuj propozycję</Button.Content>
+                  <Button.Content visible>Zatwierdź propozycję</Button.Content>
                 </Button>
               ) : (
-                <Button positive size="small" disabled={this.state.isAccepted}>
-                  <Button.Content visible>Akceptuj propozycję</Button.Content>
+                <Button
+                  positive
+                  size="small"
+                  disabled={this.state.isAccepted}
+                  onClick={this.handleAcceptEventPlace}
+                >
+                  <Button.Content visible>Zatwierdź propozycję</Button.Content>
                 </Button>
               )}
             </Grid.Row>
             <Grid.Row>
-              <Button negative size="small">
-                <Button.Content visible>Usuń obiekt</Button.Content>
-              </Button>
+              <FormControlLabel
+                color="black"
+                disabled={!this.state.isAccepted}
+                control={
+                  <GreySwitch
+                    color="grey"
+                    checked={this.state.isArchived}
+                    onChange={this.handleToggleArchiveEventPlace}
+                  />
+                }
+                label={<Typography color="textPrimary">Archiwizuj</Typography>}
+              />
             </Grid.Row>
           </GridColumn>
         </GridRow>
--- src/EventPlace/EventPlaceDetailsForm.js
+++ src/EventPlace/EventPlaceDetailsForm.js
@@ -3,6 +3,7 @@ import { Grid, Segment} from "semantic-ui-react";
 import { validation } from "../Register/validationRules"
 import { Input, Form, Typography } from "antd";
 import ToggleButton from "@material-ui/lab/ToggleButton";
+import Tooltip from "@material-ui/core/Tooltip";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import { Icon } from "@iconify/react";
 import bxBasketball from "@iconify/icons-bx/bx-basketball";
@@ -27,65 +28,80 @@ const toggleSportsCategories = [
   {
     value: "1",
     icon: soccer11,
+    tooltip: "Piłka nożna",
   },
   {
     value: "2",
     icon: volleyballIcon,
+    tooltip: "Siatkówka",
   },
   {
     value: "3",
     icon: basketball11,
+    tooltip: "Koszykówka",
   },
   {
     value: "4",
     icon: bxBasketball,
+    tooltip: "Piłka halowa",
   },
   {
     value: "6",
     icon: tennisIcon,
+    tooltip: "Tenis ziemny - single",
   },
   {
     value: "7",
     icon: tennisIcon,
     additional: "x2",
+    tooltip: "Tenis ziemny - debel",
   },
   {
     value: "8",
     icon: tennisIcon,
     icon2: LineOutlined,
+    tooltip: "Squash",
   },
   {
     value: "5",
     icon: roundSportsHandball,
+    tooltip: "Piłka ręczna",
   },
   {
     value: "9",
     icon: badmintonIcon,
+    tooltip: "Badminton - single",
   },
   {
     value: "10",
     icon: badmintonIcon,
     additional: "x2",
+    tooltip: "Badminton - debel",
   },
   {
     value: "11",
     icon: roundDirectionsRun,
+    tooltip: "Bieganie",
   },
   {
     value: "12",
     icon: roundDirectionsBike,
+    tooltip: "Kolarstwo",
   },
   {
     value: "13",
     icon: swimming15,
+    tooltip: "Pływanie",
   },
   {
     value: "14",
     icon: pingPongRacket,
+    tooltip: "Tenis stołowy",
   },
   {
     value: "15",
     icon: climbingIcon,
+    tooltip: "Wspinaczka",
   },
 ];
 
@@ -154,17 +170,21 @@ class EventPlaceDetailsForm extends Component {
                     flex: 1,
                     flexDirection: "row",
                     flexWrap: "wrap",
-                    "justify-content": "center",
-                    "align-items": "center",
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                   value={this.props.sportsCategory}
                   onChange={this.props.handleToggle}
                 >
-                  {toggleSportsCategories.map((item) => (
-                    <ToggleButton value={item.value}>
-                      <Icon icon={item.icon} width="3em" height="3em" />
-                      {item.additional}
-                    </ToggleButton>
+                  {toggleSportsCategories.map((item, key) => (
+                      <ToggleButton
+                        value={item.value}
+                        tooltip={item.tooltip}
+                        key={key}
+                      >
+                        <Icon icon={item.icon} width="3em" height="3em" />
+                        {item.additional}
+                      </ToggleButton>
                   ))}
                 </ToggleButtonGroup>
               </FormItem>
